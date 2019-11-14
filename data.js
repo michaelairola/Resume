26979e55196d6ed4677d3133f204f37a0406cfc5
michaelairola <mikeairola@mac.com>
2019-11-13 23:42:16 -0800

    hey it works I guess

---
+++ data.js
@@ -0,0 +1,43 @@
+// DATA CONFIGURATION FOR TEMPLATE
+export const infoData = {
+	Name: "Michael Airola",
+	Email: 'mik3airol@gmail.com',
+	Cell: '(831) 210-9163',
+	Address: '425 Purisima Ave, 94086 Sunnyvale CA'
+}
+export const professionalSummary = () => 
+`<b>Full stack engineer at <a href="https://defie.co/">Defie.co</a> (July 2018 - present) </b>
+<br><br>
+Cloud-Based Business Management Software Company. Specific 
+focus on customer, engineering and accounting Management tools.
+Current stack: FE - Javascript, BE - GoLang, DB - SQL`
+;
+export const workHistory = () => 'Full Stack software developer for FQ Systems';
+export const education = () => 'B.S. Mathematics, Physics Minor from UC Santa Barbara 2016';
+export const skillsData = {
+	"Component-Based Development": { 
+		description: `Increased productivity of work flow 
+		by implementing a reusable component-based system, along with the Redux
+		state management framework for state and property values.
+		`
+	},
+	"Deployment Operations": {
+		description: `Led team in refactoring the deployment pipeline, both
+		for local use with docker, as well as on the cloud with Kubernetes.`,
+	},
+	"Database Modeling": {
+		description: `By communicating with CEO, crafted clean and precise
+		database models for many different business management solutions as well as 
+		software specific engineering models.
+		`,
+	},
+	"Languages currently experienced in:": {
+		description: `Javascript, GoLang, SQL, Bash, Python, Ruby, and Rust.`
+	}, 
+	"Frameworks/Tools in current work stack:": {
+		description:		
+		`Docker, kubectl (kubernetes), git, Redux, LitElement/HtmlElement, 
+		polymer web-components and npm.`
+	}
+};
+
--- script.js
+++ script.js
@@ -19,62 +19,75 @@ const infoData = {
 	Address: '425 Purisima Ave, 94086 Sunnyvale CA'
 }
 const professionalSummary = () => 
-`<b>Full-stack Engineer at <a href="https://defie.co/">Defie.co</a> (July - present) </b>
-<br><br>
-Cloud-Based Business Management Software. Specific 
-focus on customer, engineering and accounting Management tools.
-Current stack: FE - Javascript, BE - GoLang, DB - SQL`
-;
-const workHistory = () => 'Software developer for FQ Systems';
-const education = () => 'B.S. Mathematics, Physics Minor from UC Santa Barbara 2016';
+`<b>Full stack engineer currently working at <a href="https://defie.co/">Defie.co</a></b>
+<br>
+<br>`;
+
 const skillsData = {
-	"Javascript": { time: "2.5 years",
-		description: `Crafted complex yet user-friendly client side rendering
-		 architectures for business related`,
+	"Front End": { 
+		description: `Firm grasp of vanilla Javascript, node, redux state management framework, and component-based lifecycle frameworks. Current tools used are Typescript, Redux, and LitElement. Using LitElement Components, elminated 59,591 lines of code.`
 	},
-	"GoLang": { time: "6 months",
-		description: `Using GORM library, implemented both business and server-side 
-		logical functions such as inventory-counting and data-querying tasks. 
-		`,
+	"Back End": {
+		description: `Understanding of server terminology and concepts (HTTP protocol, APIs, CRUD applications, and authentication). Using Golang, created of CRUD Router Factory that eliminated around 28,625 lines of code.`,
 	},
-	"SQL": { time: "1 year", 
-		description: `Database modeling and querying for 
-			business-to-business application, as well as data-analytics 
-			projects on the side. Specific database name 
-			used for work: 'RoachSQL'`,
+	"Dev Ops": {
+		description: `Expertise is in the Google Kubernetes Engine. Specific knowledge in: Google cloud console (gcloud), Kubernetes, Istio, Helm. Using Helm charts, simplified and speed of the deployment experience from hours to minutes.`,
 	},
-	"Redux": { time: "6 months",
-		description: `Within a month of first software job, unified a 
-		decentralized state management system into a pure redux ecosystem.`,
-	},
-	"Systems Operations": { time: "2 years",
-		description: `Bash, Git, Node, npm, Docker, Kubernetes,
-		Istio, Redux, S3, and Google Cloud knowledge.`,
-	},
-	"HTML/CSS/JS": { time: "4 years",
-		description: `Resume made with browser-friendly languages only :)`,
-	},
+	"Database": {
+		description: `Relational Database Management geek. Currently transitioning dialects from postgreSQL to MySql. Open to learning other DBMS's, relational or otherwise.`
+	}, 
+	// "System Admin": {
+	// 	description: `Knowledge of linux operating system and bash terminal environment. I love grep-ing and awk-ing my way through the terminal.`
+	// },
+	"Take Aways:": {
+		description: 'Over 7 months as lead engineer, total lines of code of our project went from 175,533 to 58,692 (65% decrease). I am dedicated to writing usable, maintainable and powerful code and am looking for an opportunity to do so.',
+	}
 };
+// const accomplishments = [
+// 	'Over 7 months as lead engineer, total lines of code of our project went from 175,533 to 58,692 (65% decrease)',
+// 	'development environment',
+// 	'CRUD http router',
+// 	'component-based architecure learning',
+// 	'signin auth system',
+// ]
+
+const education = () => 'B.S. Mathematics, Physics Minor from UC Santa Barbara 2016';
+const workHistoryData = [
+	'Full Stack Engineer at <a href="https://defie.co/">Defie.co</a>. Fremont, CA. 2018-Present.',
+	'Server at BeachSide Bar and Cafe. Santa Barbara, CA. 2016-2018.',
+	'Poke Bar manager at Big Eye Raw Bar. Santa Barbara, CA. 2017-2018.',
+	'Blenders In the grass smoothie-shack. Santa Barbara, CA. 2013-2014.',
+	'Camp Counselor at Clover Sports Camp. Carmel, CA. Summers 2009-2015.',
+]
+
 
 // templates 
+const workHistory = () => {
+	let response = '<ul class="workList">';
+	workHistoryData.forEach(i => {
+		response += '<li class="workItem">' + i +'</li>'
+	})
+	response += "</ul>"
+	return response
+}
+
 const skills = () => {
 	let response = '<ul class="xpList">';
 	for (let i in skillsData) {
-		response += `<li class="xpItem"><div><span class="xpName">${i}</span><span class="xpTime">(${skillsData[i].time})</span></div>
+		response += `<li class="xpItem"><div><span class="xpName">${i}</span><span class="xpTime">${skillsData[i].time || ''}</span></div>
 						<div class="xpDescription"><ul><li>${skillsData[i].description}</li></ul></div></li>`
 	}
 	response += "</ul>"
 	return response;
 }
 const info = () => `
-<div id="name">${infoData.Name}</div>
-<div id=""><span id="cell">${infoData.Cell}</span> | <span id="email">${infoData.Email}</span></div>
-<div id="address">${infoData.Address}</div>
+	<div id="name">${infoData.Name}</div>
+	<div id=""><span id="cell">${infoData.Cell}</span> | <span id="email">${infoData.Email}</span></div>
+	<div id="address">${infoData.Address}</div>
 `;
 
 const smallCircleContainer = () => `<div class="circle small-circle"></div>`;
 const largeCircleContainer = () => `<div class="circle large-circle"><text id="initials">MA</text></div>`;
-
 
 const setResumeMargins = margins => {
 	document.getElementById('resume').style.margin = margins;
@@ -110,7 +123,7 @@ window.onload = function() {
 	renderById(info)
 	renderById(professionalSummary)
 	renderById(skills);
-	// renderById(workHistory)
+	renderById(workHistory)
 	renderById(education)
 
 	// For printing the page properly
@@ -124,5 +137,3 @@ window.onload = function() {
 		setRowWidth("110%");
 	}
 }
-
-
