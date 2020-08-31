bc5bdc50368f6edf7253d09caf8e0a568bf34174
michaelairola <mikeairola@mac.com>
2020-08-31 12:32:45 -0700

    fix: change address, update pdf

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const infoData = {
 	Name: "Michael S Airola",
 	Email: 'mik3airola@gmail.com',
 	Cell: '831-210-9163',
-	Address: '425 Purisima Avenue. Sunnyvale CA, 94086.'
+	Address: '645 Harrison Terrace. San Jose, CA 95125.'
 }
 
 export const professionalSummary = 
@@ -19,19 +19,19 @@ export const professionalSummary =
 
 export const skillsData = new htmlObj({
 	"Front End": [
-		`Firm grasp of most javascript concepts, from plain vanilla coding to component-based lifecycle frameworks (i.e. React).`
+		`Firm grasp of most javascript concepts, from plain vanilla coding to component-based lifecycle frameworks (i.e. React). 2 years professional experience.`
 	],
 	"Back End": [
-		`Strong understanding of APIs using HTTP and HTTPS. Current Server-side languages used in production are Node and GoLang, but open to learning others.`
+		`Strong understanding of APIs using HTTP and HTTPS. Current Server-side languages used in production are Node and GoLang, but open to learning others. 2 years professional experience.`
 	],
 	"Development Operations": [
-		`Expertise is in the Google Kubernetes Engine. Specific knowledge in: Google cloud console (gcloud), Kubernetes, Istio, Helm.`
+		`Expertise is in the Google Kubernetes Engine. Specific knowledge in: Google cloud console (gcloud), Kubernetes, Istio, Helm. 2 years professional experience.`
 	],
 	"Database": [
-		`Relational Database Management geek. Currently transitioning dialects from postgreSQL to MySql. Open to learning other Data Base Management Systems, relational or otherwise. Most recent work in schema and data  migrational systems.`
+		`Relational Database Management geek. Currently transitioning dialects from postgreSQL to MySql. Open to learning other Data Base Management Systems, relational or otherwise. Most recent work in schema and data  migrational systems. 2 years professional experience.`
 	],				
 	"Achievements": [
-		`Over the past year, I have implemented many application-wide framework refactorizations that have significantly increased developer productivity and code simplicity. Finding solutions to problems is what I do best.`,
+		`Over the past 2 years, I have implemented many application-wide framework refactorizations that have significantly increased developer productivity and code simplicity. Finding solutions to problems is what I do best.`,
 		`As lead engineer, total lines of code of main work project went from 134,244 to 43,460 (67.62% decrease) within a timespan of 6 months.`,
 	],
 });
