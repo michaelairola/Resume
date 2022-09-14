c02b6aad39b1edd41f9586d9fc52962b132111e8
Michael Airola <me@michaelairola.com>
2022-09-14 13:04:42 -0700

    add ci/cd and cloud dev as different skills

--- data/software-engineer.js
+++ data/software-engineer.js
@@ -16,28 +16,28 @@ export const infoData = {
 	Address: '119 S Voluntario St. Santa Barbrara, CA 93103'
 }
 
-export const professionalSummary = 
+export const professionalSummary =
 	`Looking for positions where I can develop useful products for important causes in a healthy work environment. I love making complex things less complex.`
 	// Current position is Business Intelligence Developer at <a href="https://www.aspirehealthplan.org/">Aspire Health Plan</a>.`;
 
 export const workXps = [
 	{
-		Name: "Business Intelligence Developer", 
-		Location: "Aspire", 
-		Link: "https://www.aspirehealthplan.org/", 
-		Time: "2021-present", 
+		Name: "Business Intelligence Developer",
+		Location: "Aspire",
+		Link: "https://www.aspirehealthplan.org/",
+		Time: "2021-present",
 		Bullets: [
 			`Developed, prepped, and maintained Aspire's <a target="_blank" href="https://www.precisely.com/glossary/enterprise-data-warehouse#:~:text=An%20enterprise%20data%20warehouse%20(EDW,server%20or%20in%20the%20cloud.">Enterprise Data Warehouse</a> (SQL database) for all of Aspire's business needs.`,
-			"Gathered requirements for and implemented logic for client-requested data reports.", 
+			"Gathered requirements for and implemented logic for client-requested data reports.",
 			"Designed, implemented, and operated a data-extraction pipeline runner - complete with monitoring, validating, and scheduling features (operations/file system program written in C++). Used for all data file exchanges going to Aspire's business partners.",
 			"Designed, implemented, and operated a data load monitoring suite. Used for all data file loads coming from Aspire's business partners."
 		]
 	},{
-		Name: "Full Stack Engineer", 
-		Location: "Defie", 
+		Name: "Full Stack Engineer",
+		Location: "Defie",
 		Link: "",//"https://defie.co/",
-		Time: "2018-2021", 
-		Bullets: [ 
+		Time: "2018-2021",
+		Bullets: [
 			`Built and shipped features for an Enterprise Resource Planning (<a target="_blank" href="https://en.wikipedia.org/wiki/Enterprise_resource_planning">ERP</a>) software.`,
 			`Designed ~40 resource-oriented data models and oversaw their lifecycle implementations.`,
 			`Maintained and restructured large legacy codebase. From 2018 - 2021, total lines of code went from 134,244 to 43,460 (66% decrease!)`
@@ -45,13 +45,14 @@ export const workXps = [
 	},
 ]
 export const skills = [
-	,`Relational database work: SQL ~ 4yrs xp.`
+	 `Relational database work: SQL ~ 4yrs xp.`
 	,`SQL Server Intergration Services (SSIS) projects: C++ ~ 1 yr xp.`
 	,`Data visualization work: Tableau, Python ~ 1 yr xp. R ~ 2 yrs xp.`
 	,`Website development: Javascript, LitElement, React ~ 2 yrs xp.`
 	,`Server-side engineering: GoLang, Node ~ 2 yrs xp.`
 	,`Development Operations: Linux, Git, Docker ~ 4yrs xp.`
-	,`CI/CD pipeline engineering: Kubernetes ~ 2 ys xp. Google Cloud Run ~ 4 yrs xp.`
+	,`Cloud development engineering: Docker Container/Kubernetes ~ 2 yrs xp. AWS S3 ~ 2 yrs xp.`
+	,`CI/CD pipeline engineering: Gitlab Jenkins pipelines, Google Cloud triggers, ect }~ 2 yrs xp. `
 ]
 
 export const education = 'B.S. Mathematics, Physics Minor from UC Santa Barbara 2016';
